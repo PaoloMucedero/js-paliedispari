4fea26ffae69b1727092c52fbbe8f55750eaffef
PaoloMucedero <p.mucedero@gmail.com>
2025-12-05 19:19:08 +0100

    Tentativi funzione

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 /* Palidroma */
 //Chiedere all’utente di inserire una parola
-const richiediNome = prompt("inserisci qui la parola:");
-console.log (richiediNome)
+const richiediParola = prompt("inserisci qui la parola:");
+function reversed (richiediParola){ 
+    };
+
+console.log (richiediParola, reversed);
+
+
+
 
 //Creare una funzione per capire se la parola inserita è palindroma /* DICHIARAZIONE */
+
+//function reversed (richiediParola){}
+
+
+
 /* function isPalindroma (richiediNome){
 const isPalindroma = 
 return isPalindroma
