54212c66125de7d6dddf33ffccd225292de5241d
PaoloMucedero <p.mucedero@gmail.com>
2025-12-05 16:45:53 +0100

    apertura Repo

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Palidroma */
+//Chiedere all’utente di inserire una parola
+const richiediNome = prompt("inserisci qui la parola:");
+console.log (richiediNome)
+
+//Creare una funzione per capire se la parola inserita è palindroma /* DICHIARAZIONE */
+/* function isPalindroma (richiediNome){
+const isPalindroma = 
+return isPalindroma
+}*/
